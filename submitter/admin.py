6d74eef44470cd12a93d0dd99319a9af561692c2
SunilCotterill <sunilcotterill@gmail.com>
2023-08-06 19:18:40 -0400

    added login and register

--- submitter/admin.py
+++ submitter/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 from django.contrib import admin
 
-from .models import User, Listing, Question, Answer, Response
+from .models import CustomUser, Listing, Question, Answer, Response
 
-admin.site.register(User)
+admin.site.register(CustomUser)
 admin.site.register(Listing)
 admin.site.register(Question)
 admin.site.register(Answer)
--- submitter/views.py
+++ submitter/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question, Answer, Listing, Response, User
+from .models import Question, Answer, Listing, Response, CustomUser
 from django.template import loader
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+
+from .forms import CreateUserForm, CustomAuthenticationForm
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return redirect('submitter:register')
 
 def submission(request, listing_id):
     latest_questions_list = Question.objects.order_by("id")
@@ -25,7 +29,7 @@ def results(request, listing_id):
     template = loader.get_template("submitter/results.html")
     responses = Response.objects.filter(listing_id=listing_id)
     unique_user_ids = responses.values_list('user_id', flat=True).distinct()
-    unique_users = User.objects.filter(id__in=unique_user_ids)
+    unique_users = CustomUser.objects.filter(id__in=unique_user_ids)
 
     context = {
         "listing_id": listing_id,
@@ -38,7 +42,7 @@ def results(request, listing_id):
 def result(request, listing_id, user_id):
     answered_ids = Response.objects.filter(listing_id=listing_id).filter(user_id=user_id).values_list('answer_id', flat=True).distinct()
     answered = Answer.objects.filter(id__in=answered_ids).values_list('id', flat=True)
-    submitter = User.objects.get(pk = user_id)
+    submitter = CustomUser.objects.get(pk = user_id)
 
     latest_questions_list = Question.objects.order_by("id")
     latest_answers_list = Answer.objects.order_by("id")
@@ -69,7 +73,7 @@ def submit(request, listing_id):
             new_response.listing = Listing.objects.get(pk = listing_id)
             new_response.question = Question.objects.get(pk = question_id)
             new_response.answer = Answer.objects.get(pk = selected_answer_id)
-            new_response.user = User.objects.get(pk = 1)
+            new_response.user = CustomUser.objects.get(pk = 1)
             new_response.save()
     # results_url = reverse("submitter:results", args=[listing_id])
     redirect_url = reverse("submitter:submission_complete", args = [listing_id])
@@ -79,3 +83,46 @@ def submission_complete(request, listing_id):
     template = loader.get_template("submitter/submission_complete.html")
     context = {"listing_id": listing_id}
     return HttpResponse(template.render(context, request))
+
+def registerPage(request):
+    if not request.user.is_authenticated:
+        form = CreateUserForm()
+
+        if request.method =="POST":
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('submitter:home')
+        
+        context = {'form': form}
+        return render(request, "submitter/register.html", context)
+    else:
+        return redirect('submitter:home')
+
+def loginPage(request):
+    form = CustomAuthenticationForm()
+
+    if request.method =="POST": 
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if request.POST['email'] and request.POST['password']:
+            email = form['email'].value()
+            password = form['password'].value()
+
+            # Authenticate using your custom backend
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('submitter:home')
+            else:
+                form.add_error(None, "Invalid credentials")
+
+    context = {'form': form}
+    return render(request, "submitter/login.html", context)
+
+def homePage(request):
+    if request.user.is_authenticated:
+        context={}
+        return render(request, "submitter/homepage.html", context)
+    else:
+        return redirect('submitter:register')
+    
