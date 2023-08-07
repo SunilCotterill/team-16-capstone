845d44f120055c0644540d08fd19ac5653813254
SunilCotterill <sunilcotterill@gmail.com>
2023-08-07 18:23:26 -0400

    added navigation and changed responses

--- submitter/urls.py
+++ submitter/urls.py
@@ -10,12 +10,13 @@ urlpatterns = [
     path("<int:listing_id>/submission", views.submission, name="submission"),
     path("<int:listing_id>/submit/", views.submit, name="submit"),
     path("<int:listing_id>/results", views.results, name="results"),
-    path("<int:listing_id>/results/<int:user_id>", views.result, name="result"),
+    path("<int:listing_id>/results/<str:email>", views.result, name="result"),
     path("<int:listing_id>/submission_complete", views.submission_complete, name ="submission_complete"),
 
     path('register/', views.registerPage, name = "register"),
     path('login/', views.loginPage, name = "login"),
 
     path('home/', views.homePage, name = "home"),
-    path('new_listing/', views.new_listing, name = "new_listing")
+    path('new_listing/', views.new_listing, name = "new_listing"),
+    path('logout/', views.logout_view, name = "logout"),
 ]
